8d30b7794dcab1e1752c69dcd6f03457d5ca0945
ankur <ankurkumar0924@gmail.com>
2025-04-29 23:48:18 +0530

    Nodemon

---
+++ nodemon_.js
@@ -0,0 +1,4 @@
+//nodemon is used for executing the program continiously
+//here if u  make any change it will run automatically
+//for running type commandn  = nodemon ./fileName    [example for this file = nodemon ./nodemon_]
+console.log("helloooo pandey");
